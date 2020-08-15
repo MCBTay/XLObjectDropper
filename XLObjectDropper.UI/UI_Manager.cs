cf7ddaba943f07d60182b2d28a77dd6719234a02
Bryan Taylor <mcbtay@gmail.com>
2020-08-15 18:06:51 -0400

    initial commit of XLObjectDropper.UI

---
+++ XLObjectDropper.UI/UI_Manager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class UI_Manager : MonoBehaviour
+{
+	public GameObject MainScreenUI;
+	public GameObject RightBumperUI;
+
+	private void Start()
+	{
+		RightBumperUI.SetActive(false);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.Joystick1Button5))
+		{
+			MainScreenUI.SetActive(false);
+			RightBumperUI.SetActive(true);
+
+			Debug.Log("Right Bumper Pressed");
+		}
+		if (Input.GetKeyUp(KeyCode.Joystick1Button5))
+		{
+			MainScreenUI.SetActive(true);
+			RightBumperUI.SetActive(false);
+
+			Debug.Log("Right Bumper Released");
+		}
+	}
+}
