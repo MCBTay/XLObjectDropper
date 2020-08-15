cf69c68d78b7faaae88d49e048db0ef583f77ab0
SqueegeeDinoToy <65366947+SqueegeeDino@users.noreply.github.com>
2020-08-14 20:11:09 -0700

    Initializing a practice UI

---
+++ McBtay Unity/Assets/Scripts/UI_Manager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Manager : MonoBehaviour
+{
+    public GameObject MainScreenUI;
+    public GameObject RightBumperUI;
+
+    private void Start()
+    {
+        RightBumperUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if  (Input.GetKey(KeyCode.Joystick1Button5))
+        {
+            MainScreenUI.SetActive(false);
+            RightBumperUI.SetActive(true);
+
+            Debug.Log("Right Bumper Pressed");
+        }
+        if (Input.GetKeyUp(KeyCode.Joystick1Button5))
+        {
+            MainScreenUI.SetActive(true);
+            RightBumperUI.SetActive(false);
+
+            Debug.Log("Right Bumper Released");
+        }
+    }
+}
