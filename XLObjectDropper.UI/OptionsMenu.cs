38f776ee41f7eae529eeb85e75b132bdf713ebf3
Bryan Taylor <mcbtay@gmail.com>
2020-08-17 08:07:19 -0400

    making a quick change to break the options menu out into it's own class.  still functions as it did previously.

---
+++ XLObjectDropper.UI/OptionsMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour
+{
+	[Header("Options Menu Elements")]
+	public GameObject SnappingToggle;
+
+	public GameObject SensitivitySlider;
+	public GameObject SaveButton;
+	public GameObject UndoButton;
+	public GameObject RedoButton;
+	public GameObject LoadButton;
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
