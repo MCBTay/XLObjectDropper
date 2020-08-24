cf41afa21b56185287f2681ce21b75fef9010dab
SqueegeeDino <65366947+SqueegeeDino@users.noreply.github.com>
2020-08-23 22:19:58 -0700

    Added AusProps

---
+++ XLObjectDropper.Unity/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    // Changed to GameObject because only the game object of the menu needs to be accessed, you can 
+    // change this to any class that inherits MonoBehaviour
+    public GameObject optionsMenu;
+    bool cursorShow = false;
+
+    // At step zero, disable the optionsMenu and hide the mouse cursor
+    private void Awake()
+    {
+        optionsMenu.SetActive(false);
+        Cursor.visible = (false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Reverse the active state every time F2 is pressed
+        if (Input.GetKeyDown(KeyCode.F2))
+        {
+            optionsMenu.SetActive(!cursorShow);
+            cursorShow = !cursorShow;
+
+            if (optionsMenu.activeSelf)
+            {
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+            }
+            else
+            {
+                Cursor.visible = false;
+            }
+        }
+
+    }
+}
---
+++ XLObjectDropper.Unity/Assets/Scripts/TimeController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering;
+using UnityEngine.Assertions;
+
+public class TimeController : MonoBehaviour
+{
+    public Animation sunAnim;
+    public Slider TODSlider;
+    public GameObject skyVolume;
+    public VolumeProfile skyVolumeProfile;
+    public GameObject[] nightLights;
+
+    private void Start()
+    {
+        Debug.Log("[Coffs-Harbor] Start");
+
+        // On start, initialize the animation, and freeze playback
+        sunAnim.Play("SunAnimation");
+        sunAnim["SunAnimation"].speed = 0;
+
+        // Disable all lights on start
+        foreach (GameObject i in nightLights)
+        {
+            i.SetActive(false);
+        }
+    }
+    public void Slider_Changed(float Slider_Value)
+    {
+        // Uncomment to debug the float of the slider value
+
+        // Debug.Log("[Coffs-Harbor] Slider Value" + Slider_Value);
+
+        // Sets the animation relative to the slider value
+        sunAnim["SunAnimation"].normalizedTime = Slider_Value;
+
+        // This all drives the lights turning on and off at certain times of day
+        if(Slider_Value <= .375f)
+        {
+            foreach (GameObject i in nightLights)
+            {
+                i.SetActive(false);
+            }
+        }
+        else
+        {
+            if(Slider_Value >= .7f)
+            {
+                foreach (GameObject i in nightLights)
+                {
+                    i.SetActive(false);
+                }
+            }
+            else
+            {
+                foreach (GameObject i in nightLights)
+                {
+                    i.SetActive(true);
+                }
+            }
+        }
+    }
+    public void VolumeTest()
+    {
+        
+        Debug.Log("Current volume overrides are: " + skyVolumeProfile.components);
+    }
+}
