3a9477c23bb644644e81f2d17787ac1b9d744818
Bryan Taylor <mcbtay@gmail.com>
2020-08-25 07:30:21 -0400

    quick update to make the bumper buttons "light up" when you press them on object selection

--- XLObjectDropper.UI/ObjectSelectionUI.cs
+++ XLObjectDropper.UI/ObjectSelectionUI.cs
@@ -10,9 +10,56 @@ namespace XLObjectDropper.UI
 		[Space(10)]
 		public GameObject ListContent;
 
+		[Header("Categories")] 
+		public GameObject BasicCategory;
+		public GameObject OtherCategory;
+		public GameObject MoreCategory;
+		public GameObject StuffCategory;
+		public GameObject TiredCategory;
+		public GameObject PacksCategory;
+
+		[Header("UserInterface")]
+		public GameObject UIButton_LB;
+		public GameObject UIButton_LB_Pressed;
+		public GameObject UIButton_RB;
+		public GameObject UIButton_RB_Pressed;
+
 		private void Start()
 		{
+			UIButton_LB.SetActive(true);
+			UIButton_RB.SetActive(true);
 
+			UIButton_LB_Pressed.SetActive(false);
+			UIButton_RB_Pressed.SetActive(false);
+		}
+
+		private void Update()
+		{
+			#region Right bumper
+			if (UIManager.Instance.Player.GetButtonDown("RB"))
+			{
+				UIButton_RB.SetActive(false);
+				UIButton_RB_Pressed.SetActive(true);
+			}
+			if (UIManager.Instance.Player.GetButtonUp("RB"))
+			{
+				UIButton_RB.SetActive(true);
+				UIButton_RB_Pressed.SetActive(false);
+			}
+			#endregion
+
+			#region Left Bumper
+			if (UIManager.Instance.Player.GetButtonDown("LB"))
+			{
+				UIButton_LB.SetActive(false);
+				UIButton_LB_Pressed.SetActive(true);
+			}
+			if (UIManager.Instance.Player.GetButtonUp("LB"))
+			{
+				UIButton_LB.SetActive(true);
+				UIButton_LB_Pressed.SetActive(false);
+			}
+			#endregion
 		}
 	}
 }
