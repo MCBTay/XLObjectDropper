2edf7a9ab1ca6676d1c7d60e5b72c4a1252af4a5
Bryan Taylor <mcbtay@gmail.com>
2020-08-29 13:04:38 -0400

    pulling extensions out into it's own file.

--- XLObjectDropper/Controllers/ObjectMovementController.cs
+++ XLObjectDropper/Controllers/ObjectMovementController.cs
@@ -406,35 +406,4 @@ namespace XLObjectDropper.Controllers
 	        PreviewObject.transform.rotation = spawnable.Prefab.transform.rotation;
         }
 	}
-
-	public static class Extensions
-	{
-		public static void ChangeLayersRecursively(this Transform trans, string name)
-		{
-			trans.gameObject.layer = LayerMask.NameToLayer(name);
-			foreach (Transform child in trans)
-			{
-				child.ChangeLayersRecursively(name);
-			}
-		}
-
-		public static void ChangeLayersRecursively(this Transform trans, int layer)
-		{
-			trans.gameObject.layer = layer;
-			foreach (Transform child in trans)
-			{
-				child.ChangeLayersRecursively(layer);
-			}
-		}
-
-		public static void ChangeLayersRecursively(this Transform trans, GameObject prefab)
-		{
-			trans.gameObject.layer = prefab.layer;
-
-			for (var i = trans.childCount - 1; i >= 0; i--)
-			{
-				trans.GetChild(i).ChangeLayersRecursively(prefab.transform.GetChild(i).gameObject);
-			}
-		}
-	}
 }
---
+++ XLObjectDropper/Extensions.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace XLObjectDropper
+{
+	public static class Extensions
+	{
+		#region Transform extensions
+		public static void ChangeLayersRecursively(this Transform trans, string name)
+		{
+			trans.gameObject.layer = LayerMask.NameToLayer(name);
+			foreach (Transform child in trans)
+			{
+				child.ChangeLayersRecursively(name);
+			}
+		}
+
+		public static void ChangeLayersRecursively(this Transform trans, int layer)
+		{
+			trans.gameObject.layer = layer;
+			foreach (Transform child in trans)
+			{
+				child.ChangeLayersRecursively(layer);
+			}
+		}
+
+		public static void ChangeLayersRecursively(this Transform trans, GameObject prefab)
+		{
+			trans.gameObject.layer = prefab.layer;
+
+			for (var i = trans.childCount - 1; i >= 0; i--)
+			{
+				trans.GetChild(i).ChangeLayersRecursively(prefab.transform.GetChild(i).gameObject);
+			}
+		}
+		#endregion
+	}
+}
