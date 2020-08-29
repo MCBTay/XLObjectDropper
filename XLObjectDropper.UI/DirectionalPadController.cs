e91a4b9a8aa8e0ccc6e101bb76d4ce8cbd9c5665
Bryan Taylor <mcbtay@gmail.com>
2020-08-29 13:28:46 -0400

    refactoring the dpad UI a little bit, adding some logic for lock cam control

--- XLObjectDropper.UI/DirectionalPadController.cs
+++ XLObjectDropper.UI/DirectionalPadController.cs
@@ -1,14 +1,21 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 namespace XLObjectDropper.UI
 {
 	public class DirectionalPadController : MonoBehaviour
 	{
+		[Header("Labels")]
+		public TMP_Text UpLabel;
+		public TMP_Text DownLabel;
+		public TMP_Text LeftLabel;
+		public TMP_Text RightLabel;
+
 		[Header("Directional Pad Pressed")]
-		public GameObject DpadUp_Pressed;
-		public GameObject DpadDown_Pressed;
-		public GameObject DpadLeft_Pressed;
-		public GameObject DpadRight_Pressed;
+		public GameObject UpPressed;
+		public GameObject DownPressed;
+		public GameObject LeftPressed;
+		public GameObject RightPressed;
 
 		void OnEnable()
 		{
@@ -27,11 +34,13 @@ namespace XLObjectDropper.UI
 
 		void SetDefaultState()
 		{
-			DpadUp_Pressed.SetActive(false);
-			DpadDown_Pressed.SetActive(false);
-			DpadLeft_Pressed.SetActive(false);
-			DpadRight_Pressed.SetActive(false);
+			UpPressed.SetActive(false);
+			DownPressed.SetActive(false);
+			LeftPressed.SetActive(false);
+			RightPressed.SetActive(false);
 		}
+
+		private bool LockCam;
 
 		void Update()
 		{
@@ -40,48 +49,50 @@ namespace XLObjectDropper.UI
 			#region Right
 			if (player.GetButtonDown("DPadX"))
 			{
-				DpadRight_Pressed.SetActive(true);
+				RightPressed.SetActive(true);
+				LockCam = !LockCam;
+				RightLabel.SetText($"LOCK CAM: <color=#3286EC>{(LockCam ? "ON" : "OFF")}");
 			}
 
 			if (player.GetButtonUp("DPadX"))
 			{
-				DpadRight_Pressed.SetActive(false);
+				RightPressed.SetActive(false);
 			}
 			#endregion
 
 			#region Left
 			if (player.GetNegativeButtonDown("DPadX"))
 			{
-				DpadLeft_Pressed.SetActive(true);
+				LeftPressed.SetActive(true);
 			}
 
 			if (player.GetNegativeButtonUp("DPadX"))
 			{
-				DpadLeft_Pressed.SetActive(false);
+				LeftPressed.SetActive(false);
 			}
 			#endregion
 
 			#region Up
 			if (player.GetButtonDown("DPadY"))
 			{
-				DpadUp_Pressed.SetActive(true);
+				UpPressed.SetActive(true);
 			}
 
 			if (player.GetButtonUp("DPadY"))
 			{
-				DpadUp_Pressed.SetActive(false);
+				UpPressed.SetActive(false);
 			}
 			#endregion
 
 			#region Down
 			if (player.GetNegativeButtonDown("DPadY"))
 			{
-				DpadDown_Pressed.SetActive(true);
+				DownPressed.SetActive(true);
 			}
 
 			if (player.GetNegativeButtonUp("DPadY"))
 			{
-				DpadDown_Pressed.SetActive(false);
+				DownPressed.SetActive(false);
 			}
 			#endregion
 		}
