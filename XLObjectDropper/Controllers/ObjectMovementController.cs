02832c07034ae64ea0f830ce1edb406e04580b4d
Bryan Taylor <mcbtay@gmail.com>
2020-09-07 17:48:30 -0400

    and another one

--- XLObjectDropper/Controllers/ObjectMovementController.cs
+++ XLObjectDropper/Controllers/ObjectMovementController.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 using UnityEngine.Rendering.HighDefinition;
 using UnityModManagerNet;
 using XLObjectDropper.EventQueue;
+using XLObjectDropper.EventQueue.Events;
 using XLObjectDropper.GameManagement;
 using XLObjectDropper.UI;
 using XLObjectDropper.UserInterface;
