4e1ada3186b8dbc0b832c6f16e9ab0845ef6d661
Bryan Taylor <mcbtay@gmail.com>
2020-09-23 23:29:03 -0400

    adding a new prefab for sticks, adding a quick and dirty "animated" stick to show movement.

---
+++ XLObjectDropper.UI/Controls/StickController.cs
@@ -0,0 +1,60 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace XLObjectDropper.UI.Controls
+{
+	public class StickController : MonoBehaviour
+	{
+		public GameObject StickButton;
+		public GameObject StickButtonPressed;
+		public TMP_Text StickButtonLabel;
+		public GameObject StickAnimatedBase;
+		public GameObject StickAnimatedOverlay;
+		public TMP_Text StickAnimatedLabel;
+		public string XAxisName;
+		public string YAxisName;
+		public string ButtonName;
+
+		private void OnEnable()
+		{
+			SetDefaultState();
+		}
+
+		private void OnDisable()
+		{
+			SetDefaultState();
+		}
+
+		private void Start()
+		{
+			SetDefaultState();
+		}
+
+		private void SetDefaultState()
+		{
+			StickButton.SetActive(true);
+			StickButtonPressed.SetActive(false);
+		}
+
+		private void Update()
+		{
+			var player = UIManager.Instance.Player;
+
+			var stick = player.GetAxis2D(XAxisName, YAxisName);
+			var scaleFactor = 2.0f;
+			StickAnimatedOverlay.transform.localPosition = new Vector3(stick.x * scaleFactor, stick.y * scaleFactor, 0.0f);
+
+			if (player.GetButtonDown(ButtonName))
+			{
+				StickButton.SetActive(false);
+				StickButtonPressed.SetActive(true);
+			}
+
+			if (player.GetButtonUp(ButtonName))
+			{
+				StickButton.SetActive(true);
+				StickButtonPressed.SetActive(false);
+			}
+		}
+	}
+}
