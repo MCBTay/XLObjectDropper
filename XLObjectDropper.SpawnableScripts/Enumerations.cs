044650c98c87282032686163ebe80a6059e97071
Bryan Taylor <mcbtay@gmail.com>
2020-09-26 12:18:14 -0400

    introducing the SpawnableScripts project

---
+++ XLObjectDropper.SpawnableScripts/Enumerations.cs
@@ -0,0 +1,21 @@
+﻿namespace XLObjectDropper.SpawnableScripts
+{
+	public class Enumerations
+	{
+		public enum Style
+		{
+			None,
+			Brick,
+			Clean,
+			Block,
+			Grungy
+		}
+
+		public enum SubStyle
+		{
+			None,
+			Left,
+			Right
+		}
+	}
+}
---
+++ XLObjectDropper.SpawnableScripts/StyleController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace XLObjectDropper.SpawnableScripts
+{
+	public class StyleController : MonoBehaviour
+	{
+		public bool ShowInObjectSelection;
+		[Space(10)]
+		public Enumerations.Style Style;
+		public Enumerations.SubStyle SubStyle;
+	}
+}
