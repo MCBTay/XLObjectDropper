270c42aec3fe6cadeea6bf3139067e34daed1e3c
Bryan Taylor <mcbtay@gmail.com>
2020-09-28 18:24:53 -0400

    removing some uncalled code

--- XLObjectDropper/Controllers/ObjectEdit/EditLightController.cs
+++ XLObjectDropper/Controllers/ObjectEdit/EditLightController.cs
@@ -1,7 +1,6 @@
 ﻿using System.Linq;
 using UnityEngine;
 using UnityEngine.Rendering.HighDefinition;
-using XLObjectDropper.UI.Controls;
 using XLObjectDropper.UI.Controls.Buttons;
 using XLObjectDropper.UI.Menus;
 
@@ -130,115 +129,6 @@ namespace XLObjectDropper.Controllers.ObjectEdit
 
 				//lightExpandable.GetComponent<Selectable>().navigation = nav;
 				//}
-			}
-		}
-
-		private static void AddIntensitySlider(Expandable expandable, Light[] lights)
-		{
-			var light = lights.First();
-
-			var hdrpLight = light.GetComponent<HDAdditionalLightData>();
-			if (hdrpLight != null)
-			{
-				hdrpLight.SetLightUnit(LightUnit.Ev100);
-
-				//float ev100Min = 15.0f;
-				//float ev100Max = 25.0f;
-				//float numSteps = 40.0f;
-
-				float intensity = Mathf.Clamp(hdrpLight.intensity, ev100Min, ev100Max);
-
-				float sliderValue = ((intensity - ev100Min) / (ev100Max - ev100Min)) * numSteps;
-
-				expandable.AddSlider("Intensity", sliderValue, 0.0f, numSteps, true, value =>
-				{
-					foreach (var lightToEdit in lights)
-					{
-						var hdrpLightToEdit = lightToEdit.GetComponent<HDAdditionalLightData>();
-
-						float newIntensity = ev100Min + ((value / ev100Max) * (ev100Max - ev100Min));
-
-						hdrpLightToEdit.SetIntensity(newIntensity, LightUnit.Ev100);
-					}
-				});
-			}
-		}
-
-		private static void AddRangeSlider(Expandable expandable, Light[] lights)
-		{
-			var light = lights.First();
-
-			var hdrpLight = light.GetComponent<HDAdditionalLightData>();
-			if (hdrpLight != null)
-			{
-				//float rangeMin = 0.0f;
-				//float rangeMax = 20.0f;
-				//float numSteps = 80.0f;
-
-				float range = Mathf.Clamp(hdrpLight.range, rangeMin, rangeMax);
-
-				float sliderValue = ((range - rangeMin) / (rangeMax - rangeMin)) * numSteps;
-
-				expandable.AddSlider("Range", sliderValue, 0.0f, numRangeSteps, true, value =>
-				{
-					foreach (var lightToEdit in lights)
-					{
-						float newRange = rangeMin + ((value / rangeMax) * (rangeMax - rangeMin));
-
-						lightToEdit.GetComponent<HDAdditionalLightData>().SetRange(newRange);
-					}
-				});
-			}
-		}
-
-		private static void AddAngleSlider(Expandable expandable, Light[] lights)
-		{
-			var light = lights.First();
-
-			var hdrpLight = light.GetComponent<HDAdditionalLightData>();
-
-			if (hdrpLight.type != HDLightType.Spot) return;
-
-			if (hdrpLight != null)
-			{
-				
-
-				float spotAngle = Mathf.Clamp(light.spotAngle, rangeMin, rangeMax);
-
-				float angle = ((spotAngle - angleMin) / (angleMax - angleMin)) * numAngleSteps;
-
-				expandable.AddSlider("Angle", angle, 0.0f, numAngleSteps, true, value =>
-				{
-					foreach (var lightToEdit in lights)
-					{
-						float newSpotAngle = angleMin + ((value / angleMax) * (angleMax - angleMin));
-						lightToEdit.spotAngle = newSpotAngle;
-					}
-				});
-			}
-		}
-
-		private static void AddColorSliders(Expandable expandable, Light[] lights)
-		{
-			var light = lights.First();
-
-			var hdrpLight = light.GetComponent<HDAdditionalLightData>();
-
-			if (hdrpLight.type != HDLightType.Spot) return;
-
-			if (hdrpLight != null)
-			{
-				float rangeMin = 0.0f;
-				float rangeMax = 255.0f;
-				float numSteps = 255.0f;
-
-				expandable.AddVector3Slider("Color", "Red", "Green", "Blue", hdrpLight.color, value =>
-				{
-					foreach (var lightToEdit in lights)
-					{
-						lightToEdit.GetComponent<HDAdditionalLightData>().color = new Color(value.x, value.y, value.z);
-					}
-				});
 			}
 		}
 	}
