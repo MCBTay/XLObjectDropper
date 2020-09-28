924e398823b2b6c122949dfbd256a502d26c492a
Bryan Taylor <mcbtay@gmail.com>
2020-09-28 18:28:51 -0400

    removing some unused code and unnecessary prefab definitions

--- XLObjectDropper.UI/Controls/Expandable.cs
+++ XLObjectDropper.UI/Controls/Expandable.cs
@@ -1,25 +1,19 @@
-﻿using System.Linq;
-using TMPro;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace XLObjectDropper.UI.Controls
 {
-	public class Expandable : MonoBehaviour, ISelectHandler, IDeselectHandler
+	public class Expandable : MonoBehaviour
 	{
 		public GameObject ExpandableItem;
 		public GameObject Properties;
 		public GameObject PropertiesListContent;
 		public Animator Animator;
 		public Button Button;
-		[HideInInspector] private bool Expanded;
+		[HideInInspector] public bool Expanded;
 		[HideInInspector] public UnityEvent onSelect;
-
-		public GameObject SliderPrefab;
-		public GameObject TogglePrefab;
-		public GameObject Vector3SliderPrefab;
 
 		private void OnEnable()
 		{
@@ -44,88 +38,6 @@ namespace XLObjectDropper.UI.Controls
 			}
 
 			GetComponent<Outline>().enabled = expandableSelected || childSelected;
-		}
-
-		public GameObject AddToggle(string text, bool isChecked, UnityAction<bool> onValueChanged = null)
-		{
-			var toggle = Instantiate(TogglePrefab, PropertiesListContent.transform);
-
-			var toggleControl = toggle.GetComponent<ToggleControl>();
-
-			toggleControl.Label.SetText(text);
-			toggleControl.Toggle.isOn = isChecked;
-
-			if (onValueChanged != null)
-			{
-				toggleControl.Toggle.onValueChanged.AddListener(onValueChanged);
-			}
-
-			//if (objectSelected != null)
-			//{
-			//	listItem.GetComponent<ObjectSelectionListItem>().onSelect.AddListener(objectSelected);
-			//}
-
-			toggle.SetActive(true);
-
-			return toggle;
-		}
-
-		public void AddSlider(string text, float value, float minValue, float maxValue, bool wholeNumbers, UnityAction<float> onValueChanged = null)
-		{
-			var slider = Instantiate(SliderPrefab, PropertiesListContent.transform);
-
-			var sliderControl = slider.GetComponent<SliderControl>();
-
-			sliderControl.Label.SetText(text);
-
-			sliderControl.Slider.wholeNumbers = wholeNumbers;
-
-			sliderControl.Slider.minValue = minValue;
-			sliderControl.Slider.maxValue = maxValue;
-
-			sliderControl.Slider.value = Mathf.Clamp(value, minValue, maxValue);
-
-			//if (objectClicked != null)
-			//{
-			//	listItem.GetComponent<Button>().onClick.AddListener(objectClicked);
-			//}
-
-			if (onValueChanged != null)
-			{
-				sliderControl.onValueChanged += onValueChanged;
-			}
-
-			slider.SetActive(true);
-		}
-
-		public void AddVector3Slider(string text, string xText, string yText, string zText, Color value, UnityAction<Vector3> onValueChanged = null)
-		{
-			var slider = Instantiate(Vector3SliderPrefab, PropertiesListContent.transform);
-
-			var sliderControl = slider.GetComponent<Vector3SliderControl>();
-
-			sliderControl.Label.SetText(text);
-
-			sliderControl.XSlider.Label.SetText(xText);
-			sliderControl.XSlider.Slider.value = value.r;
-
-			sliderControl.YSlider.Label.SetText(yText);
-			sliderControl.YSlider.Slider.value = value.g;
-
-			sliderControl.ZSlider.Label.SetText(zText);
-			sliderControl.ZSlider.Slider.value = value.b;
-
-			//if (objectClicked != null)
-			//{
-			//	listItem.GetComponent<Button>().onClick.AddListener(objectClicked);
-			//}
-
-			if (onValueChanged != null)
-			{
-				sliderControl.onValueChanged += onValueChanged;
-			}
-
-			slider.SetActive(true);
 		}
 
 		public void OnSelect(BaseEventData eventData)
--- XLObjectDropper/Controllers/ObjectEditController.cs
+++ XLObjectDropper/Controllers/ObjectEditController.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
 using XLObjectDropper.Controllers.ObjectEdit;
+using XLObjectDropper.UI.Controls;
+using XLObjectDropper.UI.Controls.Buttons;
 using XLObjectDropper.UI.Menus;
 
 namespace XLObjectDropper.Controllers
@@ -32,7 +34,38 @@ namespace XLObjectDropper.Controllers
 			EditGrindablesController.AddGrindablesOptions(SelectedObject, ObjectEdit);
 
 			if (ObjectEdit.ListContent.transform.childCount > 0)
-				EventSystem.current.SetSelectedGameObject(ObjectEdit.ListContent.transform.GetChild(0).gameObject);
+			{
+				var firstChild = ObjectEdit.ListContent.transform.GetChild(0).gameObject;
+
+					var animator = firstChild?.GetComponent<Animator>();
+
+					if (animator != null)
+					{
+						animator.Play("Expand");
+					}
+
+					var expandable = firstChild?.GetComponent<Expandable>();
+
+					if (expandable != null)
+					{
+						expandable.Properties.SetActive(true);
+						expandable.Expanded = true;
+
+						var styles = firstChild.GetComponent<StyleSettingsExpandable>();
+						var lights = firstChild.GetComponent<LightSettingsExpandable>();
+						var grindables = firstChild.GetComponent<GrindableSettingsExpandable>();
+
+						var selectedObj =
+							styles != null ?
+								expandable.PropertiesListContent.transform.GetChild(0).gameObject :
+								(lights != null ? lights.EnabledToggle.gameObject :
+									(grindables != null ? grindables.GrindablesToggle.gameObject : null));
+
+						EventSystem.current.SetSelectedGameObject(selectedObj);
+					
+				}
+				//EventSystem.current.SetSelectedGameObject(ObjectEdit.ListContent.transform.GetChild(0).gameObject);
+			}
 		}
 
 		private void Update()
