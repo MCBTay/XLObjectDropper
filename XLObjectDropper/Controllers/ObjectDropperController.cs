0c8e6e3a5d08c9f03c440e18358aac7baf0aa658
Bryan Taylor <mcbtay@gmail.com>
2020-09-27 20:13:50 -0400

    destroy object edit when we destroy all the other stuff

--- XLObjectDropper/Controllers/ObjectDropperController.cs
+++ XLObjectDropper/Controllers/ObjectDropperController.cs
@@ -56,6 +56,7 @@ namespace XLObjectDropper.Controllers
 			DestroyOptionsMenu();
 			DestroyObjectMovement();
 			DestroyQuickMenu();
+			DestroyObjectEdit();
 			DestroyRotationAndScaleUI();
 			DestroySnappingModeUI();
 		}
