f4812ecca0446a3329cddffd9fa5980e8b52fd98
Bryan Taylor <mcbtay@gmail.com>
2020-09-30 07:43:42 -0400

    adding levelName and dateModified, as well as a default constructor

--- XLObjectDropper/Utilities/Save/LevelSaveData.cs
+++ XLObjectDropper/Utilities/Save/LevelSaveData.cs
@@ -7,6 +7,16 @@ namespace XLObjectDropper.Utilities.Save
 	public class LevelSaveData
 	{
 		public string levelHash;
+		public string levelName;
+		public DateTime dateModified;
 		public List<GameObjectSaveData> gameObjects;
+
+		public LevelSaveData()
+		{
+			levelHash = string.Empty;
+			levelName = string.Empty;
+			dateModified = DateTime.MinValue;
+			gameObjects = new List<GameObjectSaveData>();
+		}
 	}
 }
