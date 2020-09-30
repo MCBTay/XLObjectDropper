f65d1f9d3c067f8b5a963e7a05ef2b708c25b14b
Bryan Taylor <mcbtay@gmail.com>
2020-09-29 23:38:19 -0400

    introducing SerializableVector3 and SerializableQuaternion to make serializing/deserializing objects a lot easier.

---
+++ XLObjectDropper/Utilities/Save/SerializableQuaternion.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+
+namespace XLObjectDropper.Utilities.Save
+{
+	/// <summary>
+	/// Since unity doesn't flag the Quaternion as serializable, we
+	/// need to create our own version. This one will automatically convert
+	/// between Quaternion and SerializableQuaternion
+	/// source: https://answers.unity.com/questions/956047/serialize-quaternion-or-vector3.html
+	/// </summary>
+	[System.Serializable]
+    public struct SerializableQuaternion
+    {
+	    public float x;
+	    public float y;
+	    public float z;
+	    public float w;
+
+	    public SerializableQuaternion(Quaternion quaternion)
+	    {
+		    x = quaternion.x;
+		    y = quaternion.y;
+		    z = quaternion.z;
+		    w = quaternion.w;
+	    }
+
+        public SerializableQuaternion(float rX, float rY, float rZ, float rW)
+        {
+            x = rX;
+            y = rY;
+            z = rZ;
+            w = rW;
+        }
+
+        public override string ToString()
+        {
+            return $"[{x}, {y}, {z}, {w}]";
+        }
+
+        public static implicit operator Quaternion(SerializableQuaternion rValue)
+        {
+            return new Quaternion(rValue.x, rValue.y, rValue.z, rValue.w);
+        }
+
+        public static implicit operator SerializableQuaternion(Quaternion rValue)
+        {
+            return new SerializableQuaternion(rValue.x, rValue.y, rValue.z, rValue.w);
+        }
+    }
+}
---
+++ XLObjectDropper/Utilities/Save/SerializableVector3.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+
+namespace XLObjectDropper.Utilities.Save
+{
+	/// <summary>
+	/// Since unity doesn't flag the Vector3 as serializable, we
+	/// need to create our own version. This one will automatically convert
+	/// between Vector3 and SerializableVector3
+	/// source: https://answers.unity.com/questions/956047/serialize-quaternion-or-vector3.html
+	/// </summary>
+	[Serializable]
+	public struct SerializableVector3
+	{
+		public float x;
+		public float y;
+		public float z;
+
+		public SerializableVector3(Vector3 vector)
+		{
+			x = vector.x;
+			y = vector.y;
+			z = vector.z;
+		}
+
+		public SerializableVector3(float rX, float rY, float rZ)
+		{
+			x = rX;
+			y = rY;
+			z = rZ;
+		}
+
+		public override string ToString()
+		{
+			return $"[{x}, {y}, {z}]";
+		}
+
+		public static implicit operator Vector3(SerializableVector3 rValue)
+		{
+			return new Vector3(rValue.x, rValue.y, rValue.z);
+		}
+
+		public static implicit operator SerializableVector3(Vector3 rValue)
+		{
+			return new SerializableVector3(rValue.x, rValue.y, rValue.z);
+		}
+	}
+}
