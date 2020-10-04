2e872e037a34382af1f1fffe09fed1b7024921c1
Bryan Taylor <mcbtay@gmail.com>
2020-10-04 15:40:00 -0400

    updating to use bumper controller prefab

--- XLObjectDropper.UI/Menus/ObjectSelectionBase.cs
+++ XLObjectDropper.UI/Menus/ObjectSelectionBase.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -16,15 +15,11 @@ namespace XLObjectDropper.UI.Menus
 		public GameObject ListContent;
 		public GameObject ListItemPrefab;
 
+		public BumperController Bumpers;
+
 		[HideInInspector] public Dictionary<T, GameObject> Categories;
 		[HideInInspector] public int CurrentCategoryIndex;
 		[HideInInspector] public UnityAction CategoryChanged = () => { };
-
-		[Header("User Interface")]
-		public GameObject UIButton_LB;
-		public GameObject UIButton_LB_Pressed;
-		public GameObject UIButton_RB;
-		public GameObject UIButton_RB_Pressed;
 
 		public Animator Animator;
 
@@ -43,18 +38,26 @@ namespace XLObjectDropper.UI.Menus
 		{
 		}
 
-		
-
 		protected void Start()
 		{
 			CurrentCategoryIndex = -1;
 			SetActiveCategory(true);
+		}
 
-			UIButton_LB.SetActive(true);
-			UIButton_RB.SetActive(true);
+		private void Update()
+		{
+			Bumpers.RightBumper.EnableButton(true);
+			Bumpers.LeftBumper.EnableButton(true);
 
-			UIButton_LB_Pressed.SetActive(false);
-			UIButton_RB_Pressed.SetActive(false);
+			if (UIManager.Instance.Player.GetButtonDown("RB"))
+			{
+				SetActiveCategory(true);
+			}
+
+			if (UIManager.Instance.Player.GetButtonDown("LB"))
+			{
+				SetActiveCategory(false);
+			}
 		}
 
 		public void SetActiveCategory(bool increment)
@@ -88,39 +91,6 @@ namespace XLObjectDropper.UI.Menus
 
 			if (ListContent.transform.childCount > 0)
 				EventSystem.current.SetSelectedGameObject(ListContent.transform.GetChild(0).gameObject);
-		}
-
-		protected void Update()
-		{
-			#region Right bumper
-			if (UIManager.Instance.Player.GetButtonDown("RB"))
-			{
-				UIButton_RB.SetActive(false);
-				UIButton_RB_Pressed.SetActive(true);
-
-				SetActiveCategory(true);
-			}
-			if (UIManager.Instance.Player.GetButtonUp("RB"))
-			{
-				UIButton_RB.SetActive(true);
-				UIButton_RB_Pressed.SetActive(false);
-			}
-			#endregion
-
-			#region Left Bumper
-			if (UIManager.Instance.Player.GetButtonDown("LB"))
-			{
-				UIButton_LB.SetActive(false);
-				UIButton_LB_Pressed.SetActive(true);
-
-				SetActiveCategory(false);
-			}
-			if (UIManager.Instance.Player.GetButtonUp("LB"))
-			{
-				UIButton_LB.SetActive(true);
-				UIButton_LB_Pressed.SetActive(false);
-			}
-			#endregion
 		}
 
 		public void ClearList()
