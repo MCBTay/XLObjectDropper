469a1dd7b1f926e724a35843779429ce0afa51c4
Bryan Taylor <mcbtay@gmail.com>
2020-10-04 15:40:46 -0400

    removing some commented out code

--- XLObjectDropper.UI/Controls/AXYBController.cs
+++ XLObjectDropper.UI/Controls/AXYBController.cs
@@ -19,37 +19,6 @@ namespace XLObjectDropper.UI.Controls
 		public GameObject BButtonGameObject;
 		[HideInInspector] public AXYBButton BButton;
 
-		//[Header("PlayStation Controller Sprites")]
-		//public Sprite PlaystationX;
-		//public Sprite PlaystationXPressed;
-		//public Sprite PlaystationSquare;
-		//public Sprite PlaystationSquarePressed;
-		//public Sprite PlaystationTriangle;
-		//public Sprite PlaystationTrianglePressed;
-		//public Sprite PlaystationCircle;
-		//public Sprite PlaystationCirclePressed;
-
-		//[Header("Xbox Controller Sprites")]
-		//public Sprite XboxX;
-		//public Sprite XboxXPressed;
-		//public Sprite XboxY;
-		//public Sprite XboxYPressed;
-		//public Sprite XboxA;
-		//public Sprite XboxAPressed;
-		//public Sprite XboxB;
-		//public Sprite XboxBPressed;
-
-		//[Header("Switch Controller Sprites")]
-		//public Sprite SwitchX;
-		//public Sprite SwitchXPressed;
-		//public Sprite SwitchY;
-		//public Sprite SwitchYPressed;
-		//public Sprite SwitchB;
-		//public Sprite SwitchBPressed;
-		//public Sprite SwitchA;
-		//public Sprite SwitchAPressed;
-
-
 		private void Awake()
 		{
 			AButton = AButtonGameObject.GetComponent<AXYBButton>();
