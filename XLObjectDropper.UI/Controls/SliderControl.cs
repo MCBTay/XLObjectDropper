1e15d3be40a8045e5dd652673b311973b1164c7c
Bryan Taylor <mcbtay@gmail.com>
2020-10-07 08:15:22 -0400

    removing unnecessary events that just muddy the code

--- XLObjectDropper.UI/Controls/SliderControl.cs
+++ XLObjectDropper.UI/Controls/SliderControl.cs
@@ -10,29 +10,5 @@ namespace XLObjectDropper.UI.Controls
 		public Slider Slider;
 		public TMP_Text Label;
 		public TMP_Text Value;
-		[HideInInspector] public event UnityAction<float> onValueChanged = (x) => { };
-
-		private void OnAwake()
-		{
-			Slider.onValueChanged.AddListener((value) =>
-			{
-				onValueChanged.Invoke(value);
-			});
-		}
-
-		private void OnEnable()
-		{
-			Slider.onValueChanged.RemoveAllListeners();
-
-			Slider.onValueChanged.AddListener((value) =>
-			{
-				onValueChanged.Invoke(value);
-			});
-		}
-
-		private void OnDisable()
-		{
-			Slider.onValueChanged.RemoveAllListeners();
-		}
 	}
 }
