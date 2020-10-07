1c4fd018804c8498d79e3759f2667ad07364ae5f
Bryan Taylor <mcbtay@gmail.com>
2020-10-07 08:29:50 -0400

    adding an interface for object edit "groups"

---
+++ XLObjectDropper/Controllers/ObjectEdit/IObjectSettings.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using XLObjectDropper.UI.Menus;
+
+namespace XLObjectDropper.Controllers.ObjectEdit
+{
+	public interface IObjectSettings
+	{
+		void AddOptions(GameObject SelectedObject, ObjectEditUI ObjectEdit);
+	}
+}
