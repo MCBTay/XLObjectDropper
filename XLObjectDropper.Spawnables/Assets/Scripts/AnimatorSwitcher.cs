b836a36ca50bc665188f50853382183d35b162ab
SqueegeeDinoToy <65366947+SqueegeeDino@users.noreply.github.com>
2020-10-10 09:46:43 -0700

    Super basic script to swap poses

---
+++ XLObjectDropper.Spawnables/Assets/Scripts/AnimatorSwitcher.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorSwitcher : MonoBehaviour
+{
+    public GameObject character;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            character.GetComponent<Animator>().Play("Armature|CrouchIdle");
+        }
+        if (Input.GetButtonDown("Fire2"))
+        {
+            character.GetComponent<Animator>().Play("Armature|StandingIdle");
+        }
+        if (Input.GetButtonDown("Fire3"))
+        {
+            character.GetComponent<Animator>().Play("Idle");
+        }
+    }
+}
