8a16284b913f932aa2c7e03abe791da00b1eca3d
Bryan Taylor <mcbtay@gmail.com>
2020-10-18 15:48:52 -0400

    removing some unnecessary complexity

--- XLObjectDropper/Controllers/ObjectEditController.cs
+++ XLObjectDropper/Controllers/ObjectEditController.cs
@@ -39,23 +39,10 @@ namespace XLObjectDropper.Controllers
 
 				if (expandable != null)
 				{
-					//TODO: It feels like there's a better way to do this.
 					var general = firstChild.GetComponent<GeneralSettingsExpandable>() == null ? null : firstChild.GetComponent<GeneralSettingsExpandable>().HideInReplays.gameObject;
-					var styles = firstChild.GetComponent<StyleSettingsExpandable>() == null ? null : expandable.PropertiesListContent.transform.GetChild(0).gameObject;
-					var lights = firstChild.GetComponent<LightSettingsExpandable>() == null ? null : firstChild.GetComponent<LightSettingsExpandable>().EnabledToggle.gameObject;
-					var grindables = firstChild.GetComponent<GrindableSettingsExpandable>() == null ? null : firstChild.GetComponent<GrindableSettingsExpandable>().GrindablesToggle.gameObject;
+					if (general == null) return;
 
-					var selectedObj = general == null 
-						? (styles == null 
-							? (lights == null 
-								? grindables 
-								: lights) 
-							: styles)
-						: general;
-
-					if (selectedObj == null) return;
-
-					var selectable = selectedObj.GetComponentInChildren<Selectable>(true);
+					var selectable = general.GetComponentInChildren<Selectable>(true);
 
 					EventSystem.current.SetSelectedGameObject(selectable.gameObject);
 					selectable.OnSelect(null);
