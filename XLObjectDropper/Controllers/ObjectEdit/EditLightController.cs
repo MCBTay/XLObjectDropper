f92597c92350e9c2a1d6de0417b1e8f654baa44a
Bryan Taylor <mcbtay@gmail.com>
2020-10-18 21:26:17 -0400

    implementing light save loading

--- XLObjectDropper/Controllers/ObjectEdit/EditLightController.cs
+++ XLObjectDropper/Controllers/ObjectEdit/EditLightController.cs
@@ -1,9 +1,10 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Rendering.HighDefinition;
-using XLObjectDropper.UI.Controls.Buttons;
 using XLObjectDropper.UI.Controls.Expandables;
 using XLObjectDropper.UI.Menus;
+using XLObjectDropper.Utilities;
 using XLObjectDropper.Utilities.Save;
 using XLObjectDropper.Utilities.Save.Settings;
 
@@ -11,9 +12,6 @@ namespace XLObjectDropper.Controllers.ObjectEdit
 {
 	public class EditLightController : IObjectSettings
 	{
-		private static EditLightController _instance;
-		public static EditLightController Instance => _instance ?? (_instance = new EditLightController());
-
 		private float ev100Min = 15.0f;
 		private float ev100Max = 40.0f;
 		private float numSteps = 100.0f;
@@ -28,6 +26,7 @@ namespace XLObjectDropper.Controllers.ObjectEdit
 
 		public bool Enabled;
 		public float Intensity;
+		public LightUnit Unit;
 		public float Range;
 		public float? Angle;
 		public Vector3 Color;
@@ -45,6 +44,8 @@ namespace XLObjectDropper.Controllers.ObjectEdit
 
 				var expandable = lightExpandable.GetComponent<LightSettingsExpandable>();
 
+				Enabled = light.enabled;
+
 				expandable.EnabledToggle.Toggle.onValueChanged.AddListener((isOn) =>
 				{
 					Enabled = isOn;
@@ -57,12 +58,12 @@ namespace XLObjectDropper.Controllers.ObjectEdit
 
 				float currentIntensity = Mathf.Clamp(hdrpLight.intensity, ev100Min, ev100Max);
 				float sliderValue = ((currentIntensity - ev100Min) / (ev100Max - ev100Min)) * numSteps;
+				Intensity = sliderValue;
+				Unit = LightUnit.Ev100;
 				expandable.IntensitySlider.Slider.value = sliderValue;
-				expandable.IntensitySlider.Value.SetText(sliderValue.ToString("N"));
 
 				expandable.IntensitySlider.Slider.onValueChanged.AddListener((intensity) =>
 				{
-					Intensity = intensity;
 					foreach (var lightToEdit in lights)
 					{
 						var hdrpLightToEdit = lightToEdit.GetComponent<HDAdditionalLightData>();
@@ -70,23 +71,24 @@ namespace XLObjectDropper.Controllers.ObjectEdit
 						float newIntensity = ev100Min + ((intensity / numSteps) * (ev100Max - ev100Min));
 
 						expandable.IntensitySlider.Value.SetText(newIntensity.ToString("N"));
-
+						Intensity = newIntensity;
+						Unit = LightUnit.Ev100;
 						hdrpLightToEdit.SetIntensity(newIntensity, LightUnit.Ev100);
 					}
 				});
 
 				float currentRange = Mathf.Clamp(hdrpLight.range, rangeMin, rangeMax);
+				Range = currentRange;
 				float rangeSliderValue = ((currentRange - rangeMin) / (rangeMax - rangeMin)) * numRangeSteps;
 				expandable.RangeSlider.Slider.value = rangeSliderValue;
 				expandable.RangeSlider.Value.SetText(rangeSliderValue.ToString("N"));
 
 				expandable.RangeSlider.Slider.onValueChanged.AddListener((range) =>
 				{
-					Range = range;
 					foreach (var lightToEdit in lights)
 					{
 						float newRange = rangeMin + ((range / numRangeSteps) * (rangeMax - rangeMin));
-
+						Range = newRange;
 						expandable.RangeSlider.Value.SetText(newRange.ToString("N"));
 
 						lightToEdit.GetComponent<HDAdditionalLightData>().SetRange(newRange);
@@ -101,22 +103,24 @@ namespace XLObjectDropper.Controllers.ObjectEdit
 				else
 				{
 					float spotAngle = Mathf.Clamp(light.spotAngle, angleMin, angleMax);
+					Angle = spotAngle;
 					float angleSliderValue = ((spotAngle - angleMin) / (angleMax - angleMin)) * numAngleSteps;
 					expandable.AngleSlider.Slider.value = angleSliderValue;
 					expandable.AngleSlider.Value.SetText($"{(int)angleSliderValue}°");
 
 					expandable.AngleSlider.Slider.onValueChanged.AddListener((angle) =>
 					{
-						Angle = angle;
 						foreach (var lightToEdit in lights)
 						{
 							float newSpotAngle = angleMin + (((angle - angleMin) / numAngleSteps) * (angleMax - angleMin));
 							lightToEdit.spotAngle = newSpotAngle;
-
+							Angle = newSpotAngle;
 							expandable.AngleSlider.Value.SetText($"{(int)newSpotAngle}°");
 						}
 					});
 				}
+
+				Color = new Vector3(hdrpLight.color.r, hdrpLight.color.g, hdrpLight.color.b);
 
 				expandable.ColorSliders.XSlider.Slider.value = hdrpLight.color.r;
 				expandable.ColorSliders.XSlider.Value.SetText(hdrpLight.color.r.ToString("N"));
@@ -139,13 +143,6 @@ namespace XLObjectDropper.Controllers.ObjectEdit
 						expandable.ColorSliders.ZSlider.Value.SetText(color.z.ToString("N"));
 					}
 				};
-
-				//var nav = lightExpandable.GetComponent<Button>().navigation;
-				//nav.mode = Navigation.Mode.Explicit;
-				//nav.selectOnDown = toggle.GetComponent<ToggleControl>().Toggle;
-
-				//lightExpandable.GetComponent<Selectable>().navigation = nav;
-				//}
 			}
 		}
 
@@ -155,10 +152,48 @@ namespace XLObjectDropper.Controllers.ObjectEdit
 			{
 				enabled = Enabled,
 				intensity = Intensity,
+				unit = Unit,
 				range = Range,
 				angle = Angle,
 				color = new SerializableVector3(Color)
 			};
+		}
+
+		public void ApplySaveSettings(GameObject selectedObject, List<ISettingsSaveData> settings)
+		{
+			if (settings == null || !settings.Any()) return;
+
+			var lightSettings = settings.OfType<LightingSaveData>().ToList();
+			if (!lightSettings.Any()) return;
+
+			var lightSetting = lightSettings.First();
+
+			Enabled = lightSetting.enabled;
+			Intensity = lightSetting.intensity;
+			Unit = lightSetting.unit;
+			Range = lightSetting.range;
+			Angle = lightSetting.angle;
+			Color = lightSetting.color;
+
+			var lights = selectedObject.GetComponentsInChildren<Light>(true);
+			if (lights == null || !lights.Any()) return;
+
+			foreach (var light in lights)
+			{
+				var hdLight = light.GetComponent<HDAdditionalLightData>();
+
+				light.enabled = Enabled;
+				hdLight.enabled = Enabled;
+				hdLight.lightUnit = Unit;
+				hdLight.intensity = Intensity;
+				hdLight.range = Range;
+				if (Angle != null)
+				{
+					hdLight.SetSpotAngle(Angle.Value);
+				}
+
+				hdLight.color = new Color(Color.x, Color.y, Color.z);
+			}
 		}
 	}
 }
